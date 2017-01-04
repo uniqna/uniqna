0300a3ef409cbc36f254ecccfcf2edbadb470096
Jeremy Philemon <jeremyphilemon@outlook.com>
2017-01-05 02:40:50 +0300

    Revert "Merge remote-tracking branch 'origin/adding-answers'"
    This reverts commit 5f3ef0c2e87c5aa0910b11aa0173580cfcd55dfb, reversing
    changes made to 4d659c4f9ab47153ce4081f52bce3e02bfd46e64.

--- threads/models.py
+++ threads/models.py
@@ -1,14 +1,5 @@
 from __future__ import unicode_literals
 
 from django.db import models
-from ask.models import question
 
 # Create your models here.
-
-
-class answer(models.Model):
-    question = models.ForeignKey(question)
-    description = models.TextField()
-
-    def __str__(self):
-        return (self.description)
