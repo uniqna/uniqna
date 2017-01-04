779bffe29150c5e6058bcec3dccc8efeb0ca80c2
Jeremy Philemon <jessie@Jessies-MacBook-Pro.local>
2017-01-05 02:40:50 +0300

    Revert "Merge remote-tracking branch 'origin/adding-answers'"
    This reverts commit 5f3ef0c2e87c5aa0910b11aa0173580cfcd55dfb, reversing
    changes made to 4d659c4f9ab47153ce4081f52bce3e02bfd46e64.

--- threads/urls.py
+++ threads/urls.py
@@ -2,6 +2,5 @@ from django.conf.urls import url
 from threads import views
 
 urlpatterns = [
-    url(r'(\d{1,5})/$', views.thread),
-    url(r'(\d{1,5})/answer/$', views.submit_answer),
+    url(r'(\d{1,5})$', views.thread),
 ]
