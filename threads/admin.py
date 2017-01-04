307cb355c68757f158fdc47d78f1131f374a5ee2
Jeremy Philemon <jeremyphilemon@outlook.com>
2017-01-04 13:21:18 +0300

    Answers now supported.
    Answers get saved in the database. Each answer’s foreign key is the
    question they’re answering to. Need to support dynamic view of answers
    when viewing the thread.

--- threads/admin.py
+++ threads/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from threads.models import answer
 
 # Register your models here.
+
+
+class answer_admin(admin.ModelAdmin):
+    list_display = ('id','question', 'description',)
+
+admin.site.register(answer, answer_admin)
--- threads/views.py
+++ threads/views.py
@@ -1,6 +1,9 @@
 from django.http import HttpResponse, Http404, HttpResponseRedirect
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from threads.forms import answer_form
+from threads.models import answer
 from ask.models import question
+
 
 
 def thread(request, thread_id):
@@ -10,6 +13,21 @@ def thread(request, thread_id):
         raise Http404()
     username = request.user.username
     question_requested = question.objects.get(id=thread_id)
+    unsubmitted_answer = answer_form()
+    question_id = question_requested.pk
     return render(request,
                   'thread_templates/thread.html',
-                  {'question': question_requested, 'username': username})
+                  {'question': question_requested,
+                   'username': username,
+                   'form': unsubmitted_answer,})
+
+
+def answer(request, question_id):
+    if request.method == 'POST' and request.POST:
+        question_answered = get_object_or_404(question, pk=question_id)
+        submitted_answer = answer_form(request.POST)
+        if submitted_answer.is_valid():
+            instance = submitted_answer.save(commit=False)
+            instance.question = question_answered
+            instance.save()
+            return HttpResponse("<p>Success!</p>")
