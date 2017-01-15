d8a93c732ae42a2486bcfa96198d6d929e908445
Jeremy Philemon <jeremyphilemon@outlook.com>
2017-01-15 22:40:06 +0530

    Added support for editing answers
    Answers can now be edited. This opens an “edit page” for editing.

--- threads/urls.py
+++ threads/urls.py
@@ -6,4 +6,6 @@ urlpatterns = [
     url(r'(\d{1,5})/answer/$', views.submit_answer),
     url(r'(\d{1,5})/delete/$', views.delete_question, name='delete_question'),
     url(r'(\d{1,5})/delete/answer/(\d{1,5})$', views.delete_answer, name='delete_answer'),
+    url(r'(\d{1,5})/edit/answer/(\d{1,5})$', views.edit_answer, name='edit_answer'),
+    url(r'(\d{1,5})/edit/answer/(\d{1,5})/submit/$', views.edit_answer_submit, name='edit_answer_submit'),
 ]
