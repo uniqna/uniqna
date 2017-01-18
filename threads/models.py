77eb094441294890f599dc40f62a6dd7d560459e
digi0ps <sriru1998@gmail.com>
2017-01-18 20:37:11 +0530

    Created timestamps for answers.
    1. Displays only the posted time if the answer is not edited.
    2. Displays both the posted and the edited time if the answer is edited.

--- threads/models.py
+++ threads/models.py
@@ -2,14 +2,29 @@ from __future__ import unicode_literals
 
 from django.db import models
 from ask.models import question
-
+from datetime import datetime;
 # Create your models here.
 
 
 class answer(models.Model):
     question = models.ForeignKey(question)
     description = models.TextField()
-    answer_author = models.CharField("Author", max_length=100, default="anonymous")
+    answer_author = models.CharField("Author", max_length=100, default="anonymous");
+    created_time = models.DateTimeField(auto_now_add=True);
+    edited_time = models.DateTimeField(auto_now_add=True, editable = True);
+    edited = models.BooleanField(default=False);
 
     def __str__(self):
-        return (self.description)
+        return (self.description);
+
+    def get_time(self):
+        t = self.created_time;
+        return "{}-{}-{} {}:{}".format(t.day, t.month, t.year, t.hour, t.minute);
+
+    def get_edited_time(self):
+        t = self.edited_time;
+        return "{}-{}-{} {}:{}".format(t.day, t.month, t.year, t.hour, t.minute);
+
+    def set_edited_time(self):
+        self.edited = True;
+        self.edited_time = datetime.now();
