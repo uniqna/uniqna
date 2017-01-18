06ea6e6f11efc0a0e76ee11300731cd4df5cfe6a
digi0ps <sriru1998@gmail.com>
2017-01-18 23:57:49 +0530

    Page for users created.
    ( Accessible at /user/username )
    Userpage contains 3 sections:
    1. Details about the user. (Right now just the username and email
    address. Will expand in future)
    2. Questions asked by the user.
    3. Answers answered by the user.
    Clicking on both the question and answer listing will redirect to the
    respective thread.

---
+++ userapp/views.py
@@ -0,0 +1,15 @@
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from django.contrib.auth.models import User;
+from ask.models import question;
+from threads.models import answer;
+# Create your views here.
+
+def UserPage(request, usr):
+    if usr=="anon":
+        return render(request, "userapp/userpage.html")
+    requested_user = get_object_or_404(User, username=usr)
+    user_questions = question.objects.filter(author=usr)
+    user_answers = answer.objects.filter(answer_author=usr)
+
+    return render(request, "userapp/userpage.html", {"user_instance": requested_user, "questions": user_questions, "answers": user_answers});
