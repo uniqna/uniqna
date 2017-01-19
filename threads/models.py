4c6b4dcd37f8eca84e409f1e656149801aec9b91
digi0ps <sriru1998@gmail.com>
2017-01-19 15:15:53 +0530

    Timestamp issue fixed. 🎉

--- threads/models.py
+++ threads/models.py
@@ -1,28 +1,27 @@
 from __future__ import unicode_literals
-
+from django.utils import timezone
 from django.db import models
 from ask.models import question
-from datetime import datetime
 # Create your models here.
 
 
 class answer(models.Model):
     question = models.ForeignKey(question)
     description = models.TextField()
-    answer_author = models.CharField("Author", max_length=100, default="anonymous")
-    created_time = models.DateTimeField(auto_now_add=True)
-    edited_time = models.DateTimeField(auto_now_add=True, editable=True)
+    answer_author = models.CharField("Author", max_length=100, default="anon")
+    created_time = models.DateTimeField(default=timezone.now)
+    edited_time = models.DateTimeField(default=timezone.now, editable=True)
     edited = models.BooleanField(default=False)
 
     def __str__(self):
         return (self.description)
 
     def get_time(self):
-        t = self.created_time
+        t = timezone.localtime(self.created_time)
         return "{}-{}-{} {}:{}".format(t.day, t.month, t.year, t.hour, t.minute)
 
     def get_edited_time(self):
-        t = self.edited_time
+        t = timezone.localtime(self.edited_time)
         return "{}-{}-{} {}:{}".format(t.day, t.month, t.year, t.hour, t.minute)
 
     def set_edited_time(self):
