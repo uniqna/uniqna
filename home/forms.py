dc7df0b7d3c6a589737a7fefad65dfb307ea5930
digi0ps <sriru1998@gmail.com>
2017-01-21 11:26:52 +0530

    Username is verified using a regex.
    Only alphabets, numbers and underscore allowed.

--- home/forms.py
+++ home/forms.py
@@ -1,5 +1,6 @@
 from django import forms
 from django.contrib.auth.models import User
+import re
 
 
 class registration(forms.Form):
@@ -12,7 +13,10 @@ class registration(forms.Form):
         username = self.cleaned_data['username']
         if User.objects.filter(username=self.cleaned_data['username']).exists():
             raise forms.ValidationError('Username "%s" is already in use.' % username)
-        return username
+        if re.match(r'^[_a-zA-Z0-9]{4,15}$', username):
+            return username
+        else:
+            raise forms.ValidationError('Username not valid.\nOnly use alphabets, numbers and underscore!!!')
 
     def clean_confirm_password(self):
         password1 = self.cleaned_data.get('password')
