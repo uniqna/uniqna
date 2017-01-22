7688dc5f2955685578021f89efccf50036adad3f
Jeremy Philemon <jeremyphilemon@outlook.com>
2017-01-22 14:05:50 +0530

    Reviewed the branch.
    Ready for merge :tada:

--- root/algorithms/popularity.py
+++ root/algorithms/popularity.py
@@ -3,9 +3,9 @@ from math import log
 
 
 def _popularity(ques):
-	scores = [x.score for x in ques.answer_set.all()]
-	scores_sum = sum(scores)
-	time_diff = timezone.now() - timezone.localtime(ques.created_time)
-	time_diff_seconds = time_diff.days * 24 * 60 * 60 + time_diff.seconds
-	P = float(scores_sum + ques.points) / float(log(time_diff_seconds, 10))
-	return P
+    scores = [x.score for x in ques.answer_set.all()]
+    scores_sum = sum(scores)
+    time_diff = timezone.now() - timezone.localtime(ques.created_time)
+    time_diff_seconds = time_diff.days * 24 * 60 * 60 + time_diff.seconds
+    P = float(scores_sum + ques.points) / float(log(time_diff_seconds, 10))
+    return P
