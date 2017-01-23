ff80948758061cc0f0bdfcfc49749f8af376a6ae
Jeremy Philemon <jeremy@MacBook-Pro.local>
2017-01-23 18:22:46 +0530

    Added support for jp 😂
    - Updated the regex

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from user import views
 
 urlpatterns = [
-    url(r'^(?P<usr>[_a-zA-Z0-9]{3,15})/$', views.UserPage, name="user"),
+    url(r'^(?P<usr>[_a-zA-Z0-9]{2,15})/$', views.UserPage, name="user"),
 ]
