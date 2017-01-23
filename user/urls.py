b9f08db8a77958b47e6e8f1ab54bc6bae7e91407
Sriram <sriru1998@gmail.com>
2017-01-23 17:36:54 +0530

    Removed mod addition and changed regex.
    User page link is throwing an exception for mod. So changed the regex to including usernames of length 3.

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,5 @@ from django.conf.urls import url
 from user import views
 
 urlpatterns = [
-    url(r'^(?P<usr>[_a-zA-Z0-9]{4,15}|mod)/$', views.UserPage, name="user"),
+    url(r'^(?P<usr>[_a-zA-Z0-9]{3,15})/$', views.UserPage, name="user"),
 ]
