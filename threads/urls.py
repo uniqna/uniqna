fcd34e23610fcc12c1d8e7213fea137283d9af54
digi0ps <sriru1998@gmail.com>
2017-01-26 13:53:50 +0530

    REST API done.
    Shows the votes of every question.

--- threads/urls.py
+++ threads/urls.py
@@ -1,8 +1,10 @@
 from django.conf.urls import url
 from threads import views
+from rest_framework.urlpatterns import format_suffix_patterns
 
 urlpatterns = [
     url(r'(\d{1,5})/$', views.thread, name='thread'),
+    url(r'answers/$', views.VotesView.as_view()),
     url(r'(\d{1,5})/answer/$', views.submit_answer),
     url(r'(\d{1,5})/delete/$', views.delete_question, name='delete_question'),
     url(r'(\d{1,5})/solved/$', views.mark_answer_solved, name='mark_answer_solved'),
@@ -11,3 +13,5 @@ urlpatterns = [
     url(r'(\d{1,5})/edit/answer/(\d{1,5})/submit/$', views.edit_answer_submit, name='edit_answer_submit'),
     url(r'(\d{1,5})/answer/(\d{1,5})/([up,down])/$', views.vote, name='vote'),
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
