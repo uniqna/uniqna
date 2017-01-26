1a02045f59ace04ffe4e7c2e227c4f3fa295eab4
digi0ps <sriru1998@gmail.com>
2017-01-26 13:09:18 +0530

    Fixed Negative Age Bug.

--- home/forms.py
+++ home/forms.py
@@ -11,7 +11,7 @@ class registration(forms.Form):
     confirm_password = forms.CharField(widget=forms.PasswordInput)
     bio = forms.CharField(max_length=240)
     location = forms.CharField(max_length=30)
-    age = forms.IntegerField()
+    age = forms.IntegerField(min_value=0, max_value=99)
     course = forms.ChoiceField(student.course_choices)
     school = forms.ChoiceField(student.school_choices)
 
