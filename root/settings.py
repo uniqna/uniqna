306d5ed99319ba71a7ad482506c6e58097f1d1da
jeremyphilemon <jeremyphilemon@users.noreply.github.com>
2017-01-29 01:28:23 +0530

    Fixed local development server connection error.

--- root/settings.py
+++ root/settings.py
@@ -93,7 +93,7 @@ else:
     DATABASES = {
         'default': {
             'ENGINE': 'django.db.backends.postgresql_psycopg2',
-            'NAME': 'ebdb',
+            'NAME': 'qnadb',
             'USER': 'jeremyphilemon',
             'PASSWORD': 'XnfvtmdWkUfDZwKKmnEcbpQJPJZYKrJmzBZenVxZQNRMbxdjtCCSZtckvR',
             'HOST': 'aa1ffwboodhyt2t.cmtq348kawjn.ap-south-1.rds.amazonaws.com',
