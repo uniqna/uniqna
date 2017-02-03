baa2fe2296444d2b54a74027660bf23c8d9325ae
jeremyphilemon <jeremyphilemon@users.noreply.github.com>
2017-02-03 19:52:13 +0530

    Fixed answer div's spacing and markdown rendering.

--- threads/views.py
+++ threads/views.py
@@ -20,6 +20,8 @@ def thread(request, thread_id):
     unsubmitted_answer = answer_form()
     question_id = question_requested.pk
     all_answers = answer.objects.filter(question=thread_id).order_by("-score")
+    for x in all_answers:
+        x.description = markdown2.markdown(x.description)
     return render(request,
                   'thread_templates/thread.html',
                   {'question': question_requested,
