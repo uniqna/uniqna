677183398f724dfc9e4620e88795338f42f0f899
digi0ps <admin@digiops.me>
2017-02-11 22:29:27 +0530

    Notification tweaks. Added method to add notification object to all user objects.

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,8 @@ from django.conf.urls import url
 from user import views
 
 urlpatterns = [
-    url(r'^(?P<usr>[_a-zA-Z0-9]{2,15})/$', views.UserPage, name="user"),
-    url(r'^(?P<usr>[_a-zA-Z0-9]{2,15})/edit/$', views.EditProfile, name="edit_profile"),
-    url(r'^(?P<usr>[_a-zA-Z0-9]{2,15})/changepassword/$', views.ChangePassword, name="change_password"),
+	url(r'^update/$', views.update_all),
+	url(r'^(?P<usr>[_a-zA-Z0-9]{2,15})/$', views.UserPage, name="user"),
+	url(r'^(?P<usr>[_a-zA-Z0-9]{2,15})/edit/$', views.EditProfile, name="edit_profile"),
+	url(r'^(?P<usr>[_a-zA-Z0-9]{2,15})/changepassword/$', views.ChangePassword, name="change_password"),
 ]
