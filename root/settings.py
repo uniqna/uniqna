214668d36ecb33fe51d70702460638a24b7531aa
jeremyphilemon <jeremyphilemon@users.noreply.github.com>
2017-02-27 23:32:49 +0530

    Security keys and credentials are now environment variables.

--- root/settings.py
+++ root/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 
 import os
 from django.conf.global_settings import TEMPLATES
+from root import secret_settings
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -19,14 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'v@v9)ij^1+r&apveb-r#sf@+=kip2el+d8izw@26c_hqksh0%0'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
 ALLOWED_HOSTS = ['*']
-
 
 # Application definition
 
@@ -125,15 +122,29 @@ else:
     STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
     # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-# EMAIL
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-EMAIL_USE_SSL = True
-EMAIL_HOST = 'mail.privateemail.com'
-EMAIL_PORT = 465
-EMAIL_HOST_USER = 'superheroes@uniqna.com'
-EMAIL_HOST_PASSWORD = 'w4B7b@5#4jR9'
-DEFAULT_FROM_EMAIL = 'superheroes@uniqna.com'
-SERVER_EMAIL = 'superheroes@uniqna.com'
+
+# Credentials
+if 'SECRET_KEY' in os.environ:
+    SECRET_KEY = os.environ['SECRET_KEY']
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_SSL = True
+    EMAIL_HOST = os.environ['EMAIL_HOST']
+    EMAIL_PORT = os.environ['EMAIL_PORT']
+    EMAIL_HOST_USER = os.environ['EMAIL_HOST_USER']
+    EMAIL_HOST_PASSWORD = os.environ['EMAIL_HOST_PASSWORD']
+    DEFAULT_FROM_EMAIL = os.environ['DEFAULT_FROM_EMAIL']
+    SERVER_EMAIL = os.environ['SERVER_EMAIL']
+else:
+    SECRET_KEY = secret_settings.SECRET_KEY
+    EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+    EMAIL_USE_SSL = True
+    EMAIL_HOST = secret_settings.EMAIL_HOST
+    EMAIL_PORT = secret_settings.EMAIL_PORT
+    EMAIL_HOST_USER = secret_settings.EMAIL_HOST_USER
+    EMAIL_HOST_PASSWORD = secret_settings.EMAIL_HOST_PASSWORD
+    DEFAULT_FROM_EMAIL = secret_settings.DEFAULT_FROM_EMAIL
+    SERVER_EMAIL = secret_settings.SERVER_EMAIL
+
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 
