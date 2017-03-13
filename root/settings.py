37d2d9e9862e198a48641c161ab4614bdd7cfa18
digi0ps <sriru1998@gmail.com>
2017-03-13 09:41:09 +0530

    Guest View in Userpage done.
    Changes:
    - Guest view in Userpage implemented
    - Moved javascript of Userpage to /static/js/user/user.js
    - Removed unecessary *.snackbar_reveals*, one is enough.

--- root/settings.py
+++ root/settings.py
@@ -128,8 +128,8 @@ else:
         }
     }
     DEBUG = True
-    STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-    # STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+    # STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
+    STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 
 # Credentials
