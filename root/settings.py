bb866e64702c58359b43280c6114b9ba65936fed
Jeremy Philemon <jeremyphilemon@outlook.com>
2017-03-29 10:45:52 +0530

    Corrected the syntax of True.

--- root/settings.py
+++ root/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = TRUE
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
