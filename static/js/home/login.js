1546c281e6ff7f06393d26da9051be18e4b06806
digi0ps <sriru1998@gmail.com>
2017-06-17 00:10:36 +0530

    Collectstatic.

--- static/js/home/login.js
+++ static/js/home/login.js
@@ -1,29 +1,37 @@
-// Field validation
-var n = new Notyf({ delay: 5000 });
-$(".login").hide();
-$('.submit').on("click", function(e) {
-    e.preventDefault();
-    flag = 1;
-    if ($("#username").val() == "") {
-        n.alert("Username is blank.")
-        flag = 0;
-    }
-    if ($("#password").val() == "") {
-        n.alert("Password is blank.")
-        flag = 0;
-    }
-    if (flag) {
-        $(".login").click();
-    }
-});
 
-//Shifting field focus
-$("#username").on("keypress", function(e){
-    // Whenever enter is pressed this shifts the focus to 
-    // password input instead of submitting
-    if (e.which == 13){
+(function() {
+
+    // Field validation
+    var n = new Notyf({ delay: 5000 });
+    $(".login").hide();
+    $('.submit').on("click", function(e) {
         e.preventDefault();
-        e.stopPropagation();
-        $("#password").focus();
-    }
-});
+        flag = 1;
+        if ($("#username").val() == "") {
+            n.alert("Username is blank.")
+            flag = 0;
+        }
+        if ($("#password").val() == "") {
+            n.alert("Password is blank.")
+            flag = 0;
+        }
+        if (flag) {
+            $(".login").click();
+        }
+    });
+
+    //Shifting field focus
+    $("#username").on("keypress", function(e){
+        // Whenever enter is pressed this shifts the focus to 
+        // password input instead of submitting
+        if (e.which == 13){
+            e.preventDefault();
+            e.stopPropagation();
+            $("#password").focus();
+        }
+    });
+
+    // Focus on username field when logged in 
+    $("#username").focus();
+
+})();
