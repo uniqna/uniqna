de883ea60711dd66c6a35dfa9352552035ae175e
Jeremy Philemon <jeremyphilemon@outlook.com>
2017-06-22 02:42:31 +0530

    Added reply author panel mockup

--- home/static/js/reply_modal.js
+++ home/static/js/reply_modal.js
@@ -1,16 +1,20 @@
+var reply_url = window.location.origin + '/thread/reply/';
+
 var reply = new Vue({
   delimiters: ["[", "]"],
-  el: '.replying-to',
+  el: '.replying',
   data: {
     username: '',
+    url: '',
   }
 });
 
 var getuser = new Vue({
   el: '.reply-vue',
   methods: {
-    giveUsername: function (dingdong) {
+    giveUsername: function (dingdong, id) {
       reply.username = dingdong;
+      reply.url = reply_url + id + '/';
       $(".reply-modal").addClass("is-active");
     }
   }
