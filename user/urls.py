f1521e9daf60c94767ebd9be8e4caee736b6c937
digi0ps <sriru1998@gmail.com>
2017-06-23 01:13:04 +0530

    Add routing in user page.
    The javascript is kinda ugly.
    But when we do a react refactor,
    this will be useless.

--- user/urls.py
+++ user/urls.py
@@ -2,7 +2,7 @@ from django.conf.urls import url
 from user import views
 
 urlpatterns = [
-    url(r'^(?P<user>[_a-zA-Z0-9]{2,15})/$', views.user_page, name="user"),
+    url(r'^(?P<user>[_a-zA-Z0-9]{2,15})(/replies)?/$', views.user_page, name="user"),
     url(r'^(?P<user>[_a-zA-Z0-9]{2,15})/edit/$', views.edit_profile, name="edit_profile"),
     url(r'^(?P<user>[_a-zA-Z0-9]{2,15})/changepassword/$', views.change_password, name="change_password"),
     url(r'^(?P<user>[_a-zA-Z0-9]{2,15})/toggle-email/$', views.toggle_email_notification, name="toggle_email"),
