d8ae0492cfef388d738be7f039529d65adc8adc0
Jeremy Philemon <jeremyphilemon@outlook.com>
2017-06-26 14:13:40 +0530

    Added support for yuglify compressor

--- root/settings.py
+++ root/settings.py
@@ -293,3 +293,6 @@ PIPELINE = {
         },
     }
 }
+
+PIPELINE['CSS_COMPRESSOR'] = 'pipeline.compressors.yuglify.YuglifyCompressor'
+PIPELINE['JS_COMPRESSOR'] = 'pipeline.compressors.yuglify.YuglifyCompressor'
