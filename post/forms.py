74006fafeec0ccb63280e3fc1662a79cf2a2c279
Jeremy Philemon <jeremyphilemon@outlook.com>
2017-07-05 17:33:50 +0530

    Refactored channels dropdown while posting (#83)

--- post/forms.py
+++ post/forms.py
@@ -1,10 +1,16 @@
 from django import forms
 
 from post.models import Question
+from post.models import Channel
 
 
 class post_form(forms.ModelForm):
+    channels = forms.ModelChoiceField(queryset=Channel.objects.filter(visible=True).order_by('name'), required=False)
 
     class Meta:
         model = Question
-        fields = ('title', 'description', )
+        fields = ('title', 'description', 'channels')
+
+    def __init__(self, *args, **kwargs):
+        super(post_form, self).__init__(*args, **kwargs)
+        self.fields["channels"].choices = [("", ""), ] + list(self.fields["channels"].choices)[1:]
