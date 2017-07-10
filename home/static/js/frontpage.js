27fbc0ccf17d1947eb9ff73d0f2f55f39b12a751
digi0ps <sriru1998@gmail.com>
2017-07-10 23:45:39 +0530

    Modify infinite_scroll.js and implement infinite scrolling in frontpage.

--- home/static/js/frontpage.js
+++ home/static/js/frontpage.js
@@ -20,12 +20,13 @@ var fp = new Vue({
   },
   methods: {
     fetchPosts: function(cat, page=1) {
+      console.log('Category: ', cat);
+      console.log('Page: ', page);
       endpoint = `/api/frontpage/${cat}/${page}/`
       var fp = this;
-      fp.loaded = false;
-      $.get(endpoint, function(data) {
+      // Return the jquery ajax object
+      return $.get(endpoint, function(data) {
         if (data){
-          fp.loaded = true;
           if (fp.category == cat)
             fp.posts = [...fp.posts, ...data]
           else
@@ -33,9 +34,22 @@ var fp = new Vue({
           fp.category = cat;
           fp.page = page;
         }
+      })
+      .done(function(data){
+        if (data.length)
+          InfiniteScroll.enable();
+      })
+      .fail(function(){
+        InfiniteScroll.enable();
+        console.error('No network connectivity.');
       });
+    },
+    infiScroll: function() {
+      this.fetchPosts(this.category, ++this.page, 3);
     }
   }
 });
 
 fp.user = $("#vue-frontpage").data('user');
+
+InfiniteScroll.init('vue-frontpage', fp.infiScroll, 5);
--- home/static/js/infinite_scroll.js
+++ home/static/js/infinite_scroll.js
@@ -1,19 +1,36 @@
-var InfiniteScroll = function(element_id, callback, threshold=7){
-  
-  // element_id - id of the element to attach the infinite scroll listener
-  // callback - function to callback
-  // threshold - more the threshold, the earlier the function will be called
-  var posts_container = document.getElementById(element_id);
-  var container_height = posts_container.offsetHeight;
-  var already_called_flag = 0;
-  window.onscroll = function() {
-    var scrollY = window.scrollY;
-    if ((scrollY + (threshold*100)) >= container_height){
-      if (!already_called_flag){
-        already_called_flag = 1;
-        callback();
-      }
-    }
-  } 
+var InfiniteScroll = (function(){
 
-}
+  exports = {};
+
+  flag = 1;
+
+  exports.init = function(element_id, callback, threshold){
+    // element_id - id of the element to attach the infinite scroll listener
+    // callback - function to callback
+    // threshold - more the threshold, the earlier the function will be called
+    window.onscroll = function() {
+      var posts_container = document.getElementById(element_id);
+      var container_height = posts_container.offsetHeight;
+      var scroll = window.scrollY + threshold*100;
+      if (scroll >= container_height){
+        if (flag){
+          flag = 0;
+          callback();
+        }
+      }
+    } 
+  }
+
+  exports.enable = function() {
+    flag = 1;
+    return flag;
+  }
+
+  exports.disable = function() {
+    flag = 0;
+    return flag;
+  }
+
+  return exports;
+
+})();
