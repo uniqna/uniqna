b5bc320b675ed31a38701377d31c38008b266f2a
jeremyphilemon <jeremyphilemon@outlook.com>
2018-10-23 19:59:52 -0400

    Updated yuglify compressors and fixed .js exceptions.

--- root/settings.py
+++ root/settings.py
@@ -367,8 +367,8 @@ PIPELINE = {
     }
 }
 
-PIPELINE['CSS_COMPRESSOR'] = 'pipeline.compressors.cssmin.CSSMinCompressor'
-PIPELINE['JS_COMPRESSOR'] = 'pipeline.compressors.uglifyjs.UglifyJSCompressor'
+PIPELINE['CSS_COMPRESSOR'] = 'pipeline.compressors.yuglify.YuglifyCompressor'
+PIPELINE['JS_COMPRESSOR'] = 'pipeline.compressors.yuglify.YuglifyCompressor'
 
 # Pagination
 EL_PAGINATION_PER_PAGE = 10
